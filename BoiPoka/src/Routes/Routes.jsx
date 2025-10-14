00d645a9341237f541841d8825849be690ff32bf
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2025-10-14 17:42:18 +0600

    1

---
+++ BoiPoka/src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import { PacmanLoader } from "react-spinners";
+import Root from "../Pages/Root/Root";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+        Component: Root,
+        children: [
+            {index:true, Component:Home}
+        ]
+  },
+  {
+    path: "*",
+    element: (
+      <div className=" mx-auto flex border h-screen items-center justify-center">
+        <p className="text-4xl font-bold text-center mr-10">Not Found</p>
+        <PacmanLoader color="#3b8dd0" loading margin={0} size={47} />
+      </div>
+    ),
+  },
+]);
