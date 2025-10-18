9934db9adcf64c12c39fbc883ed2bcf64b83b5f0
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2025-10-18 17:29:16 +0600

    3

--- HomeDecoration/src/Pages/Home/Home.jsx
+++ HomeDecoration/src/Pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
+import ProductCard from '../../Components/productcard/ProductCard';
+import useProducts from '../../Hooks/useProducts';
 
 const Home = () => {
     const data = useLoaderData();
-    console.log(data);
+    const hookData = useProducts();
+    console.log(hookData);
+    const fetureData = data.slice(0, 6);
+    console.log(fetureData);
+    // console.log(data);
+
     return (
         <div>
-            Home
+            <div className='flex justify-between items-center py-5'>
+                <h1 className='text-2xl font-semibold'>Fetured Product</h1>
+                <Link to={"/products"} className='btn outline'>See All Products</Link>
+            </div>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 '>
+                {
+                    fetureData.map(product=><ProductCard key={product.id} product={product}></ProductCard>)
+                }
+            </div>
         </div>
     );
 };
